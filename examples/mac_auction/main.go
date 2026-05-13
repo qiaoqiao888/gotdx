140e3f1e2326e7a40b683fe7eb10a903a1aff8e9
abulo.hoo <abulo.hoo@gmail.com>
2026-05-13 14:19:57 +0800

    refactor(cmd): 移除webviewer中未使用的query_date参数
    移除cmd/webviewer/query.go中未被使用的queryDate解析逻辑，
    因为request中的query_date参数仍然会通过其他方式传递。
    
    BREAKING CHANGE: webviewer的runMethod函数不再解析query_date参数

--- examples/mac_auction/main.go
+++ examples/mac_auction/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"log"
 
-	"github.com/bensema/gotdx"
 	"github.com/bensema/gotdx/examples/internal/exampleutil"
+	"github.com/bensema/gotdx/types"
 )
 
 func main() {
 	client := exampleutil.NewMACClient()
 	defer client.Disconnect()
 
-	items, err := client.MACAuction(gotdx.MarketSZ, "000001", 0, 20)
+	items, err := client.MACAuction(types.MarketSZ.Uint8(), "000001", 0, 20)
 	if err != nil {
 		log.Fatalln(err)
 	}
