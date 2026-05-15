4af7fb9cc359ae8fc1f494e61fe2b8753368a036
abulo.hoo <abulo.hoo@gmail.com>
2026-05-16 00:43:21 +0800

    refactor(examples): 更新示例代码中的市场类型引用
    - 将 gotdx.MarketSZ 和 gotdx.MarketSH 替换为 types.MarketSZ.Uint8() 和 types.MarketSH.Uint8()
    - 添加 github.com/bensema/gotdx/types 导入语句
    - 移除不再使用的 github.com/bensema/gotdx 导入语句
    - 统一使用 types 包中的市场类型定义

--- examples/mac_transactions/main.go
+++ examples/mac_transactions/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"log"
 
-	"github.com/bensema/gotdx"
 	"github.com/bensema/gotdx/examples/internal/exampleutil"
+	"github.com/bensema/gotdx/types"
 )
 
 func main() {
 	client := exampleutil.NewMACClient()
 	defer client.Disconnect()
 
-	items, err := client.MACTransactions(gotdx.MarketSZ, "000001", 0, 20)
+	items, err := client.MACTransactions(types.MarketSZ.Uint8(), "000001", 0, 20)
 	if err != nil {
 		log.Fatalln(err)
 	}
